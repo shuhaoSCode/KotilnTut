78dbc09f33def4961c310bfef94c1d1a017ae05d
luke <luke.w@topskyweb.com>
2017-09-08 20:59:49 +0800

    init commit

--- src/demo/KotlinHut.kt
+++ src/demo/KotlinHut.kt
@@ -8,15 +8,53 @@ fun main(args: Array<String>) {
     //可变
     var sex = "male"
 
-    var bigInt :Int = Int.MAX_VALUE
-    var smallInt:Int = Int.MIN_VALUE
+    var bigInt: Int = Int.MAX_VALUE
+    var smallInt: Int = Int.MIN_VALUE
 
-    println("Biggest Int : "+ bigInt)
+    println("Biggest Int : " + bigInt)
     println("smallInt Int : $smallInt")
 
-    if (sex is String){
+    if (sex is String) {
         println("true")
-    }else{
+    } else {
         println("false")
     }
+
+    println("3.14 to Int : " + (3.14.toInt()))
+    println("A to Int : " + ('A'.toInt()))
+    println("65 to Char : " + (65.toChar()))
+
+    //-----
+
+    val myName = "Luke W"
+    val longStr = """This is a long String"""
+
+    var fName: String = "Luke"
+    var lName: String = "W"
+
+    fName = "Sally"
+
+    var fullName = "$fName $lName"
+    println("Name : $fullName")
+
+    println("1 + 2 = ${1 + 2}")
+    println("String Length =  ${longStr.length}")
+
+    var str1 = "A random String"
+    var str2 = "a random String"
+
+    println("Strings Equal : ${str1.equals(str2)}")
+    println("Compare A to B : ${"A".compareTo("B")}")
+
+    println("2nd Index : ${str1.get(2)}")
+    // or println("2nd Index : ${str1[2]}")
+
+    println("Index 2 - 7 : ${str1.subSequence(2, 8)}")
+    println("Contains random : ${str1.contains("random")} ")
+
+
+
+    //----
+
+
 }
--- src/demo/KotlinHut2.kt
+++ src/demo/KotlinHut2.kt
@@ -2,5 +2,58 @@ package demo
 
 fun main(args: Array<String>) {
 
+    /*var myArray = arrayOf(1, 1.23, "luke")
+    println(myArray[2])
+    myArray[1] = 3.14
+    println("Array Size : ${myArray.size}")
+    println("Luke in Array : ${myArray.contains("luke")}")
+
+    var partArray = myArray.copyOfRange(0, 1)
+
+    println("First : ${myArray.first()}")
+    println("Luke Index : ${myArray.indexOf(" luke")}")
+
+    var sqArray = Array(5, { x -> x * x })
+    println(sqArray[3])
+    println(sqArray.size)
+
+    var arrayInt: Array<Int> = arrayOf(1, 2, 3)*/
+
+    /*val oneTo10 = 1..10;
+    val alpha = "A".."Z"
+    println("R in Alpha : ${"R" in alpha}")
+
+    val tenTo1 = 10.downTo(1)
+    val twoTo20 = 2.rangeTo(20)
+    val rng3 = oneTo10.step(3)
+    for (x in rng3) println("rng3 : $x")
+    for (x in tenTo1.reversed()) println("tenTo1 : $x")*/
+
+    /*var age = 100
+
+    when (age) {
+        0, 1, 2, 3, 4, 5 -> println("幼稚园")
+
+        in 6..12 -> {
+            val grade = age - 5
+            println("小学${grade}年级")
+        }
+
+        in 13..15 -> {
+            val grade = age - 12
+            println("初$grade")
+        }
+
+        in 16..18 -> {
+            val grade = age - 15
+            println("高$grade")
+        }
+
+        else -> println("大学")
+    }*/
+
+    for (x in 1..100)
+        println("Loop : $x")
+
 
 }
